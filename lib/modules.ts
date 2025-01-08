c1199e011d13288eca8ef2ede8357acf0f441677
kajalanS <kajalan4@gmail.com>
2025-01-09 02:50:12 +0530

    first commit

---
+++ lib/modules.ts
@@ -0,0 +1,8 @@
+import { Module } from "../types/modules";
+
+export default function modules(name: string): Module {
+  const data: Module = {
+    data: "name"
+  }
+  return data;
+}
---
+++ types/modules.d.ts
@@ -0,0 +1,5 @@
+
+
+export interface Module {
+    data: string;
+}
