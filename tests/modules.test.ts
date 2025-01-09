2e18601c04922e51915e1722aea4eb92b091599d
Kajalan <103587022+kajalanS@users.noreply.github.com>
2025-01-09 16:55:19 +0530

    Update modules.test.ts

--- tests/modules.test.ts
+++ tests/modules.test.ts
@@ -1,6 +1,9 @@
-
-
+import modules from "../lib/modules"
+import { Module } from "../types/modules"
 
 test('should expect true all time', () => {
-    expect("done").toStrictEqual("done")
+    const data: Module = {
+        data: "name"
+    }
+    expect(modules("name")).toStrictEqual(data)
 })
