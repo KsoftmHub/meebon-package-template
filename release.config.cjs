3d2872a34067705d4cdcfeb385e1dcbe9606016c
kajalanS <kajalan4@gmail.com>
2025-01-12 15:02:16 +0530

    Fix: update version in package.json to 1.0.0 and add semantic release plugins in release.config.cjs

--- release.config.cjs
+++ release.config.cjs
@@ -8,4 +8,11 @@ export default {
       prerelease: true
     },
   ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/changelog", { "changelogFile": "CHANGELOG.md" }],
+    ["@semantic-release/git", { "assets": ["CHANGELOG.md"] }],
+    "@semantic-release/github"
+  ]
 };
